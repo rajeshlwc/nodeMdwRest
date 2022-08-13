12992a9189e5e6bc750ee40133d710fecc5388f7
Rajesh Ramachandran <rajesh.ramachandran@salesforce.com>
2022-08-13 21:15:50 +0530

    updated to use pe logger

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var channel = process.env.PEChannel || '/event/SendToSAP__e';
 var user = process.env.SFUserName || 'rajesh@herodev.org';
 var password = process.env.SFPwdAndToken;
 console.log(password);
-var replayId = -1;
+var replayId =  process.env.PEReplayId || -1;
 var interfaceName = process.env.InterfaceName || 'SAPInterface';
 
 //parameters to publish response
@@ -24,11 +24,12 @@ conn.login(user,password,function(err, userInfo){
     if (err) {return console.log(err);}
     console.log("login successful");
     //get replay Id by querying interface tracker object
-    conn.query("select Id,Last_Replay_Id__c from Integration_Tracker__c where name = '" + interfaceName + "' order by Last_replay_id__c desc limit 1", function(err, result){
+    //conn.query("select Id,Last_Replay_Id__c from Integration_Tracker__c where name = '" + interfaceName + "' order by Last_replay_id__c desc limit 1", function(err, result){
+    conn.query("select Id,Replay_Id__c from PE_Logger__c where PE_Name__c = '" + interfaceName + "' order by Replay_Id__c desc limit 1", function(err, result){
         if (err){return console.error(err);}
         if (result.records){
             console.log(result.records);
-            replayId = parseInt(result.records[0].Last_Replay_Id__c);
+            replayId = parseInt(result.records[0].Replay_Id__c);
             var integTrackerId = result.records[0].Id;
 
             //create streaming client
@@ -46,8 +47,10 @@ conn.login(user,password,function(err, userInfo){
               var subscription = client.subscribe(channel, function(message){
                   console.log(message.event.replayId);
                   console.log(message.payload.Message__c);
+                  console.log(message.payload.Unique_Id__c);
                   var CelciusObj = message.payload.Message__c;
                   console.log(typeof(CelciusObj));
+                  var uniqueId = message.payload.Unique_Id__c;
 
 
                   //call rest api for testing
@@ -61,7 +64,8 @@ conn.login(user,password,function(err, userInfo){
                           console.log("record created " + ret.success);
                       });
                       //update processed replay id on integration tracker
-                      conn.sobject("Integration_Tracker__c").insert({Name: interfaceName,Last_Replay_Id__c: message.event.replayId},function(err, rec){
+                    //  conn.sobject("Integration_Tracker__c").insert({Name: interfaceName,Last_Replay_Id__c: message.event.replayId},function(err, rec){
+                     conn.sobject("PE_Logger__c").upsert({Status__c: "Status__c",Replay_Id__c: message.event.replayId},uniqueId, function(err, rec){
                           if (err || !rec.success) {return console.error(err,rec);}
                           console.log("tracker updated");
                       })
@@ -70,6 +74,10 @@ conn.login(user,password,function(err, userInfo){
                     })
                     .catch(error => {
                       console.error(error);
+                      conn.sobject("PE_Logger__c").upsert({Exception__c: error.message,Replay_Id__c: message.event.replayId},uniqueId, function(err, rec){
+                           if (err || !rec.success) {return console.error(err,rec);}
+                           console.log("tracker updated");
+                       })
 
                     });
               });
